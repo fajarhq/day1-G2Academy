e7d23739c31d05402e6ab2e012a314718f8725bd
fajarhq <fajarhq@gmail.com>
2020-08-10 21:48:34 +0700

    home n about

---
+++ script.js
@@ -0,0 +1,13 @@
+const pages = ["homePage", "aboutPage"]
+
+function changePageTo(idPage) {
+    for (let i = 0; i < pages.length; i++) {
+        let page = document.getElementById(pages[i]).classList;
+        if(idPage == pages[i]){
+            page.remove('hide-page')
+            continue
+        }
+        page.add('hide-page')
+    } 
+}
+changePageTo("homePage")
